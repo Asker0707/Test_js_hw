d93f4720481e5f5fc5b11facb2547afa57f3ca0b
Asker0707 <asker_original@mail.ru>
2025-02-11 22:13:45 +0300

    eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,25 @@
-module.exports = {
-    'env': {
-      'browser': true,
-      'es2021': true,
-      'node': true
+// eslint.config.js
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig({
+  languageOptions: {
+    globals: {
+      // Определите глобальные переменные здесь
+      window: 'readonly',
+      document: 'readonly',
+      // другие глобальные переменные, если нужно
     },
-    'extends': 'eslint:recommended',
-    'overrides': [
-      {
-        'files': ['**/*/*.test.js'],
-        'plugins': ['jest'],
-        'extends': ['plugin:jest/recommended'],
-        'rules': { 'jest/prefer-expect-assertions': 'off' }
-      }
-    ],
-    'parserOptions': {
-      'ecmaVersion': 'latest',
-      'sourceType': 'module'
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
     },
-    'rules': {}
-  }
+  },
+  // Другие настройки
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended', // если вы используете React
+  ],
+  rules: {
+    // ваши правила
+  },
+});
